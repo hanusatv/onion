16732069c7a4e35165503b2cc377e4d8595d78fc
Hanus á Tjaldrafløtti Vestergaard <hanusatv@gmail.com>
2022-09-21 02:54:16 +0200

    first commit

---
+++ db/createtables.sql
@@ -0,0 +1,24 @@
+-- SQLite
+    BEGIN;
+    CREATE TABLE semesters (
+        semester_id NOT NULL PRIMARY KEY,
+        code text NOT NULL,
+        Description text NOT NULL);
+
+
+    CREATE TABLE courses (
+        course_id text NOT NULL PRIMARY KEY,
+        description text NOT NULL,
+        semester integer,
+        ECTS real,
+        FOREIGN KEY (semester)
+            REFERENCES semesters (course_id));
+
+    CREATE TABLE exams (
+        exam_id integer PRIMARY KEY AUTOINCREMENT,
+        description text NOT NULL,
+        course text NOT NULL,
+        date text
+    );
+
+    COMMIT;
---
+++ db/dbconnector.py
@@ -0,0 +1,30 @@
+import sqlite3
+from pathlib import Path
+
+connection = sqlite3.connect('onion.db')
+
+cursor = connection.cursor()
+
+file_create_tables = Path('db/createtables.sql').read_text()
+
+cursor.executescript(file_create_tables)
+
+connection.commit()
+
+
+semesters = [
+    (1, '2022H', 'Høst 2022'),
+    (2, '2023V', 'Vår 2023'),
+    (3, '2023H', 'Høst 2023'),
+    (4, '2023H', 'Vår 2024'),
+    (5, '2023H', 'Høst 2024'),
+    (6, '2023H', 'Vår 2025')
+]
+
+cursor.executemany("INSERT INTO semesters VALUES(?,?,?)", semesters)
+
+cursor.execute("SELECT * FROM semesters")
+
+print(cursor.fetchall())
+
+connection.close()
